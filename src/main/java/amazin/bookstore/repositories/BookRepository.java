9e5f628a7d85a7fdf15033daf1899eb5849bb122
thedana1 <danaelsherif1@gmail.com>
2023-11-05 16:21:41 -0500

    Book Entity and Controller. Basic thymeleaf

---
+++ src/main/java/amazin/bookstore/repositories/BookRepository.java
@@ -0,0 +1,29 @@
+package amazin.bookstore.repositories;
+
+import amazin.bookstore.Book;
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.CrudRepository;
+
+/***
+ * Repository for books
+ * @author Dana El Sherif
+ */
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+    //Used for search functionality
+    List<Book> findByIsbnOrDescriptionOrAuthorOrPublisherOrTitle(String isbn, String description, String author, String publisher, String title);
+
+
+    //Below functions might be used by other classes. If not used in future implementations, they can be deleted.
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitle(String title);
+    Book findById(long id);
+    Book findByIsbn(String isbn);
+
+    List<Book> findByPublisher(String publisher);
+
+    Object findAll(Sort title);
+}

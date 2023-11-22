e19510b5943e2d944dd8ed41b2a9964ad97e0e60
Yaw <yawasamoah13@hotmail.com>
2023-11-22 04:28:12 -0500

    Flyway implementation. Will need tests for BookController and UserController to be updated to pass multiple times.

--- src/main/resources/db/migration/V1__Create_Book_Table.sql
+++ src/main/resources/db/migration/V1__Create_Book_Table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE if not exists Book (
-                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                      isbn VARCHAR(255) NOT NULL,
-                      title VARCHAR(255) NOT NULL,
-                      author VARCHAR(255) NOT NULL,
-                      publisher VARCHAR(255) NOT NULL,
-                      description VARCHAR(1000) NOT NULL
+CREATE TABLE if not exists "book" (
+        "id" BIGINT AUTO_INCREMENT PRIMARY KEY,
+        "author" VARCHAR(255) NOT NULL,
+        "description" VARCHAR(1000) NOT NULL,
+        "isbn" VARCHAR(255) NOT NULL,
+        "publisher" VARCHAR(255) NOT NULL,
+        "title" VARCHAR(255) NOT NULL
+
 );

dcfc8dd4e754fefb7822bdf7914b48c018e8fa98
thedana1 <danaelsherif1@gmail.com>
2023-11-26 21:05:43 -0500

    fixed unit tests

--- src/test/java/TestBook.java
+++ src/test/java/TestBook.java
@@ -1,109 +1,129 @@
-//import amazin.bookstore.User;
-//import org.junit.jupiter.api.Test;
-//import amazin.bookstore.Book;
-//import org.junit.jupiter.api.BeforeEach;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-///**
-// * Tests the Book Class
-// */
-//
-//class TestBook {
-//
-//    private Book book;
-//
-//    /**
-//     * Initializing a new book before each test
-//     * to ensure methods work
-//     */
-//    @BeforeEach
-//    public void initialize() {
-//        book = new Book("12345", "Red Dragon", "Jacob B", "Pub Corp", "fantasy" );
-//    }
-//
-//    /**
-//     * Tests get ISBN method of book
-//     */
-//    @Test
-//    void getIsbn() {
-//        assertEquals("12345", book.getIsbn());
-//    }
-//
-//    /**
-//     * Tests set ISBN method of book
-//     */
-//    @Test
-//    void setIsbn() {
-//        book.setIsbn("22222");
-//        assertEquals("22222", book.getIsbn());
-//    }
-//
-//    /**
-//     * Tests get title method of book
-//     */
-//    @Test
-//    void getTitle() {
-//        assertEquals("Red Dragon", book.getTitle());
-//    }
-//
-//    /**
-//     * Tests set title method of book
-//     */
-//    @Test
-//    void setTitle() {
-//        book.setTitle("Cloudy");
-//        assertEquals("Cloudy", book.getTitle());
-//    }
-//
-//    /**
-//     * Tests get author method of book
-//     */
-//    @Test
-//    void getAuthor() {
-//        assertEquals("Jacob B", book.getAuthor());
-//    }
-//
-//    /**
-//     * Tests set author method of book
-//     */
-//    @Test
-//    void setAuthor() {
-//        book.setAuthor("Bob");
-//        assertEquals("Bob", book.getAuthor());
-//    }
-//
-//    /**
-//     * Tests get publisher method of book
-//     */
-//    @Test
-//    void getPublisher() {
-//        assertEquals("Pub Corp", book.getPublisher());
-//    }
-//
-//    /**
-//     * Tests set publisher method of book
-//     */
-//    @Test
-//    void setPublisher() {
-//        book.setPublisher("Publishing");
-//        assertEquals("Publishing", book.getPublisher());
-//    }
-//
-//    /**
-//     * Tests get description method of book
-//     */
-//    @Test
-//    void getDescription() {
-//        assertEquals("fantasy", book.getDescription());
-//    }
-//    /**
-//     * Tests set description method of book
-//     */
-//    @Test
-//    void setDescription() {
-//        book.setDescription("Horror");
-//        assertEquals("Horror", book.getDescription());
-//
-//    }
-//}
+import amazin.bookstore.User;
+import org.junit.jupiter.api.Test;
+import amazin.bookstore.Book;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests the Book Class
+ */
+
+class TestBook {
+
+    private Book book;
+
+    /**
+     * Initializing a new book before each test
+     * to ensure methods work
+     */
+    @BeforeEach
+    public void initialize() {
+        book = new Book("12345", "Red Dragon", "Jacob B", "Pub Corp", "fantasy", 12.5 );
+    }
+
+    /**
+     * Tests get ISBN method of book
+     */
+    @Test
+    void getIsbn() {
+        assertEquals("12345", book.getIsbn());
+    }
+
+    /**
+     * Tests set ISBN method of book
+     */
+    @Test
+    void setIsbn() {
+        book.setIsbn("22222");
+        assertEquals("22222", book.getIsbn());
+    }
+
+    /**
+     * Tests get title method of book
+     */
+    @Test
+    void getTitle() {
+        assertEquals("Red Dragon", book.getTitle());
+    }
+
+    /**
+     * Tests set title method of book
+     */
+    @Test
+    void setTitle() {
+        book.setTitle("Cloudy");
+        assertEquals("Cloudy", book.getTitle());
+    }
+
+    /**
+     * Tests get author method of book
+     */
+    @Test
+    void getAuthor() {
+        assertEquals("Jacob B", book.getAuthor());
+    }
+
+    /**
+     * Tests set author method of book
+     */
+    @Test
+    void setAuthor() {
+        book.setAuthor("Bob");
+        assertEquals("Bob", book.getAuthor());
+    }
+
+    /**
+     * Tests get publisher method of book
+     */
+    @Test
+    void getPublisher() {
+        assertEquals("Pub Corp", book.getPublisher());
+    }
+
+    /**
+     * Tests set publisher method of book
+     */
+    @Test
+    void setPublisher() {
+        book.setPublisher("Publishing");
+        assertEquals("Publishing", book.getPublisher());
+    }
+
+    /**
+     * Tests get description method of book
+     */
+    @Test
+    void getDescription() {
+        assertEquals("fantasy", book.getDescription());
+    }
+    /**
+     * Tests set description method of book
+     */
+    @Test
+    void setDescription() {
+        book.setDescription("Horror");
+        assertEquals("Horror", book.getDescription());
+
+    }
+
+    /**
+     * Tests get price method of book
+     */
+    @Test
+    void getPrice() {
+        assertEquals(12.5, book.getPrice());
+
+    }
+
+    /**
+     * Tests set price method of book
+     */
+    @Test
+    void setPrice() {
+        book.setPrice(3.00);
+        assertEquals(3.00, book.getPrice());
+
+    }
+
+}
